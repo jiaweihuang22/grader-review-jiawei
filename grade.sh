5f095a08af17418afc5e770de76aa6fdcf335134
jiaweihuang22 <130082278+jiaweihuang22@users.noreply.github.com>
2023-11-28 10:38:10 -0800

    Add files via upload

---
+++ grade.sh
@@ -0,0 +1,32 @@
+CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+
+rm -rf student-submission
+rm -rf grading-area
+
+mkdir grading-area
+
+git clone $1 student-submission
+echo 'Finished cloning'
+
+
+# Draw a picture/take notes on the directory structure that's set up after
+# getting to this point
+
+# Then, add here code to compile and run, and do any post-processing of the
+# tests
+if [[ -f student-submission/ListExamples.java ]] 
+then
+    echo 'File Exists'
+    
+else
+    echo 'File does not exist'
+    exit
+fi
+
+cp *.java grading-area
+cp -r lib grading-area
+cp student-submission/ListExamples.java grading-area
+
+cd grading-area
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"  *.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
