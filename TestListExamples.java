9f62c671e39cc016944cb7b690bac580e8d81991
jiaweihuang22 <130082278+jiaweihuang22@users.noreply.github.com>
2023-11-28 10:42:57 -0800

    Add files via upload

---
+++ TestListExamples.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.Arrays;
+import java.util.List;
+
+class IsMoon implements StringChecker {
+  public boolean checkString(String s) {
+    return s.equalsIgnoreCase("moon");
+  }
+}
+
+public class TestListExamples {
+  @Test(timeout = 500)
+  public void testMergeRightEnd() {
+    List<String> left = Arrays.asList("a", "b", "c");
+    List<String> right = Arrays.asList("a", "d");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter() {
+    List<String> a = Arrays.asList("moon","haha","MOON","lol","mOoN");
+    List<String> b = ListExamples.filter(a,new IsMoon());
+    List<String> expected = Arrays.asList("moon","MOON","mOoN");
+    assertEquals(expected, b);
+  }
+}
